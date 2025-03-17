6fff898f1202fba16dba6be7c04a1cb45427f0ec
DHARANIvg <kaveevgdharani@gmail.com>
2025-03-17 21:22:09 +0530

    Import Libraries

---
+++ Hospital_Waste_Prediction_Dharani/routes.py
@@ -0,0 +1,8 @@
+from flask import request, jsonify, render_template
+import numpy as np
+import pandas as pd
+import joblib
+from  Hospital_Waste_Prediction_Dharani import hospital_bp
+from datetime import datetime
+
+
