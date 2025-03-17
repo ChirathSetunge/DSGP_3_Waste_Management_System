a39edfaa77e81ff8bd9d919de30c64c2336580e7
DHARANIvg <kaveevgdharani@gmail.com>
2025-03-17 21:30:55 +0530

    load model

--- Hospital_Waste_Prediction_Dharani/routes.py
+++ Hospital_Waste_Prediction_Dharani/routes.py
@@ -6,3 +6,5 @@ from  Hospital_Waste_Prediction_Dharani import hospital_bp
 from datetime import datetime
 
 
+model = joblib.load("Hospital_Waste_Prediction_Dharani/ml_model/best_XGB_model.pkl")
+le = joblib.load("Hospital_Waste_Prediction_Dharani/ml_model/label encoder.pkl")
