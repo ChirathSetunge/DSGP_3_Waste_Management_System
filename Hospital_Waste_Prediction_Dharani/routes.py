c0597e3c42d519f2755bfbb14d68fb897f1b5c1e
DHARANIvg <kaveevgdharani@gmail.com>
2025-03-17 21:33:35 +0530

    preprossing methods

--- Hospital_Waste_Prediction_Dharani/routes.py
+++ Hospital_Waste_Prediction_Dharani/routes.py
@@ -19,3 +19,17 @@ def categorize_rain(precipitation):
         return 'moderate'
     else:
         return 'heavy'
+
+def preprocess_input(data):
+
+    try:
+        date_obj = datetime.strptime(data['date'], "%Y-%m-%d")
+        data['cat_rain'] = categorize_rain(data['precipitation_sum'])
+        data['Day of the week'] = date_obj.weekday()
+        data['Month'] = date_obj.month
+        data['Day'] = date_obj.day
+        data['cat_rain'] = le.transform([data['cat_rain']])[0]
+
+        return [[data['Day of the week'], data['Month'], data['Day'], data['cat_rain'], data['daily_patients']]]
+    except Exception as e:
+        return str(e)
