73e6f869d9a9b6fd5563eb035f8e4483dfb1661f
DHARANIvg <kaveevgdharani@gmail.com>
2025-03-17 21:32:18 +0530

    categorizing precipitation methods

--- Hospital_Waste_Prediction_Dharani/routes.py
+++ Hospital_Waste_Prediction_Dharani/routes.py
@@ -8,3 +8,14 @@ from datetime import datetime
 
 model = joblib.load("Hospital_Waste_Prediction_Dharani/ml_model/best_XGB_model.pkl")
 le = joblib.load("Hospital_Waste_Prediction_Dharani/ml_model/label encoder.pkl")
+
+def categorize_rain(precipitation):
+
+    if precipitation < 2.5:
+        return 'no'
+    elif precipitation < 7.5:
+        return 'light'
+    elif precipitation < 15:
+        return 'moderate'
+    else:
+        return 'heavy'
