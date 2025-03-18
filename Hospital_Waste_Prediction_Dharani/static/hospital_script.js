04162955f480516190dcc7f2a7daa2ade1a25d23
DHARANIvg <kaveevgdharani@gmail.com>
2025-03-18 09:02:52 +0530

    Add event listner

--- Hospital_Waste_Prediction_Dharani/static/hospital_script.js
+++ Hospital_Waste_Prediction_Dharani/static/hospital_script.js
@@ -1,3 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
     const predictBtn = document.getElementById("predictBtn");
     const predictionResult = document.getElementById("predictionResult");
+    predictBtn.addEventListener("click", function () {
