ed5e8d00f3424f5151baf432f75b60973a6e1d38
DHARANIvg <kaveevgdharani@gmail.com>
2025-03-18 09:04:14 +0530

    get input values

--- Hospital_Waste_Prediction_Dharani/static/hospital_script.js
+++ Hospital_Waste_Prediction_Dharani/static/hospital_script.js
@@ -1,4 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const predictBtn = document.getElementById("predictBtn");
     const predictionResult = document.getElementById("predictionResult");
+
     predictBtn.addEventListener("click", function () {
+
+
+        const date = document.getElementById("dumpDate").value;
+        const precipitation = parseFloat(document.getElementById("precipitation").value);
+        const dailyPatients = parseInt(document.getElementById("dailyPatients").value);
+
+        if (!date || isNaN(precipitation) || isNaN(dailyPatients)) {
+            predictionResult.innerHTML = "<span class='text-danger'>Please enter valid inputs.</span>";
+            return;
+        }
+    }
