bf0eddcc86a42ba340efb800f509c2cce8e4e68b
HIMAN WITHANA 🇱🇰 <154534130+Don-Withana@users.noreply.github.com>
2025-03-29 17:16:22 +0530

    Add blue_print files_chatbot

---
+++ Feedback_Complaints_Chatbot_Himan/__init__.py
@@ -0,0 +1,12 @@
+from flask import Blueprint
+from Feedback_Complaints_Chatbot_Himan.chat.intent_database import db
+
+chatbot_bp = Blueprint('chatbot', __name__, template_folder='templates', static_folder='static')
+
+from Feedback_Complaints_Chatbot_Himan import routes
+
+
+def init_chatbot_db(app):
+    with app.app_context():
+        db.create_all()
+        print("Database tables created successfully")
